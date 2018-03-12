f55c6ef836e52e3d314d32e3151ef8e488456bb6
Jonathan Luk <jluk87@gmail.com>
2018-03-12 22:32:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect {|song| song.name =name} || self.new_by_name(name)
+    self.find_by_name(name) || self.new_by_name(name)
   end
 
   def self.alphabetical
