07c24f5c2f5b2f86032792bf7a2aeafd22f53191
Jonathan Luk <jluk87@gmail.com>
2018-03-12 21:55:42 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def initialize
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create
+
+
+end
+ 
